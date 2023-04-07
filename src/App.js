a0523722e69d5ab6baa4176c7137b887750a19e9
wkc0509 <wkc05098@gmail.com>
2023-04-07 11:15:11 +0900

    #5 로그인페이지 구성

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import LoginPage from "./page/LoginPage"
 import './App.css';
 import MainPage from "./page/MainPage";
-
+import RegisterPage from "./page/RegisterPage";
+import LoginPage2 from "./page/LoginPage2"
 
 function App() {
     return (
@@ -11,6 +12,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<MainPage/>}/>
                 <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/register" element={<RegisterPage/>}/>
+                <Route path="/login2" element={<LoginPage2/>}/>
             </Routes>
         </BrowserRouter>
     );
--- src/page/LoginPage.js
+++ src/page/LoginPage.js
@@ -1,18 +1,29 @@
 import {Content} from "antd/es/layout/layout";
 import styles from "../component/LoginPage.module.css";
+import {useNavigate} from "react-router-dom";
 
 const LoginPage = (props) => {
+    const navigate = useNavigate();
+
+
+    const goRegisterPage = () => {
+        navigate('../RegisterPage');
+    }
+    const goLoginPage = () => {
+        navigate('../LoginPage2');
+    }
+
     return (
         <Content>
             <div className={styles.login}>
                 <b>컨테이너의 모든 것</b>
                 <h1>Everything of Container</h1>
                 <br/>
-                <button className={styles.btn1}>관리자 LOGIN</button>
+                <button className={styles.btn1} onClick={goLoginPage}>관리자 LOGIN</button>
                 <br/><br/>
-                <button className={styles.btn1}> 사용자 LOGIN</button>
+                <button className={styles.btn1} onClick={goLoginPage}> 사용자 LOGIN</button>
                 <br/><br/>
-                <button className={styles.btn1}>SIGN UP</button>
+                <button className={styles.btn1} onClick={goRegisterPage}>SIGN UP</button>
             </div>
         </Content>
     )
