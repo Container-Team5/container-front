45b26e416b70168e0f76290ff7a94d002aa5d5d0
wkc0509 <wkc05098@gmail.com>
2023-04-21 10:22:51 +0900

    #8 로그인페이지와 회원가입페이지 구현 (80% 완성)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import LoginPage from "./page/LoginPage"
 import './App.css';
 import MainPage from "./page/MainPage";
-import RegisterPage from "./page/RegisterPage";
+import LoginPage from "./page/LoginPage"
 import LoginPage2 from "./page/LoginPage2"
+import Register from "./page/Register"
+
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<MainPage/>}/>
-                <Route path="/login" element={<LoginPage/>}/>
-                <Route path="/register" element={<RegisterPage/>}/>
-                <Route path="/login2" element={<LoginPage2/>}/>
+
+                <Route path="/account" element={<LoginPage/>}/>
+                <Route path="/account/Login" element={<LoginPage2/>}/> /*관리자,사용자 로그인 */
+
+                <Route path="/account/Register" element={<Register/>}/>
+
             </Routes>
         </BrowserRouter>
     );
--- src/page/LoginPage.js
+++ src/page/LoginPage.js
@@ -1,29 +1,50 @@
 import {Content} from "antd/es/layout/layout";
 import styles from "../component/LoginPage.module.css";
 import {useNavigate} from "react-router-dom";
+import user_login from "../user-login.png";
+import user_register from "../user-register.png";
+
 
 const LoginPage = (props) => {
     const navigate = useNavigate();
 
-
     const goRegisterPage = () => {
-        navigate('../RegisterPage');
+        navigate('./Register');
     }
     const goLoginPage = () => {
-        navigate('../LoginPage2');
+        navigate('./Login');
+    }
+    const goAccountPage = () => {
+        navigate('../account');
     }
 
     return (
         <Content>
-            <div className={styles.login}>
-                <b>컨테이너의 모든 것</b>
-                <h1>Everything of Container</h1>
-                <br/>
-                <button className={styles.btn1} onClick={goLoginPage}>관리자 LOGIN</button>
-                <br/><br/>
-                <button className={styles.btn1} onClick={goLoginPage}> 사용자 LOGIN</button>
-                <br/><br/>
-                <button className={styles.btn1} onClick={goRegisterPage}>SIGN UP</button>
+            <div className={styles.page_wrapper}>
+                <header className={styles.page_header}>
+                    <h1 onClick={goAccountPage}>Everything Of Container</h1>
+                    <nav>
+                        <span onClick={goLoginPage}>로그인</span>
+                        <span> | </span>
+                        <span onClick={goRegisterPage}>회원가입</span>
+                    </nav>
+                </header>
+
+                <div className={styles.page_middle}>
+                    <div className={styles.middle_login}>
+                        <img src={user_login}/>
+                        <h2>관리자/사용자 로그인</h2>
+                        <button className={styles.middle_button} onClick={goLoginPage}>로그인</button>
+                    </div>
+                    <div className={styles.middle_register}>
+                        <img src={user_register}/>
+                        <h2>관리자/사용자 회원가입</h2>
+                        <button className={styles.middle_button} onClick={goRegisterPage}>회원가입</button>
+                    </div>
+                </div>
+                <footer className={styles.page_footer}>
+                    <h3>컨테이너의 모든 것</h3>
+                </footer>
             </div>
         </Content>
     )
